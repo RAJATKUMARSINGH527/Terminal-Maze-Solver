2e816b1131cea27e1c9081eb5dfa1f7151dbb1f4
RAJATKUMARSINGH527 <rajatkumarsingh257@gmail.com>
2024-01-20 00:08:42 +0530

    some error fix

--- GenerateMaze.py
+++ GenerateMaze.py
@@ -2,4 +2,6 @@ import random
 
 def generate_maze(n):
     maze = [['▓' if random.random() < 0.25 else '◌' for _ in range(n)] for _ in range(n)]
+    maze[0][0] = "S"
+    maze[n-1][n-1] = "E"
     return maze
